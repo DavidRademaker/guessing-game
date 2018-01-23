2ca7dc91718eadb7840ec8b65fadce6f3b5f119f
David Rademaker <davidrademaker@Interapts-MacBook-Pro-97.local>
2018-01-23 15:45:00 -0500

    first commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by David Rademaker on 1/23/18.
+//  Copyright © 2018 David Rademaker. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
