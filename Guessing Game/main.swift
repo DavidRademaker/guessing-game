b96bba84a3d17cc5494ff3b22c724c2e12acbcf7
David Rademaker <davidrademaker@Interapts-MacBook-Pro-97.local>
2018-01-24 13:23:24 -0500

    outlined project

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -8,5 +8,22 @@
 
 import Foundation
 
-print("Hello, World!")
+//generate random number
+var randomNumber = arc4random_uniform(100)
+//ask for input
+print("Guess a number 1-100")
+//take input
+var guess = readLine()
+//determine if wrong or right   loop until corect
 
+//if correct ask if would like to play again
+
+//if incorrect tell answer then ask to play again
+
+
+
+
+if guess = randomNumber {
+print("You Got It")
+
+}
